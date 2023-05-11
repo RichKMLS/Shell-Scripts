d4fb96d4d16a005e2cf059309d7031002a85c323
Rich K. MLS <105183376+RichKMLS@users.noreply.github.com>
2023-05-11 22:50:03 +0200

    Search for a file fast.

---
+++ megasearch.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# This script prompts the user for a search term and a directory to search in,
+# then performs a case-insensitive search for files and directories with names
+# that match the search term. The script prints the last modification time and
+# file path for each file found, sorted by modification time from newest to oldest.
+#
+# Usage:
+# 1. Run the script.
+# 2. Enter the search term when prompted (use regex if desired).
+# 3. Enter the directory to search in when prompted (leave blank to search the entire filesystem).
+#
+# Example:
+# $ ./search.sh
+# Search term: *.txt
+# Directory to search in: /home/user/documents
+#
+# The above example searches for all .txt files in the /home/user/documents directory
+# and prints the last modification time and file path for each file found, sorted by
+# modification time from newest to oldest.
+
+read -p "Search term: " searchTerm
+read -p "Directory to search in: " searchDir
+
+if [ -z "$searchDir" ]; then
+    searchDir="/"
+fi
+
+find $searchDir -iname $searchTerm -print0 2>/dev/null | \
+	xargs -0 ls -lt --time-style=long-iso | awk '{print $6,$7,$8,$9}'
+
